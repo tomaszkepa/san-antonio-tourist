b26ea77accdd5653393cd6489812fc3668e73baa
tomaszkepa <tomasz.kepa@live.com>
2017-08-30 08:12:47 +0200

    feat(SAT-12): Add redux logger

--- src/main/store.js
+++ src/main/store.js
@@ -1,11 +1,30 @@
-import { createStore as createBasicStore } from 'redux';
+import { createStore as createBasicStore, applyMiddleware } from 'redux';
 import { combineReducers } from 'redux-immutable';
+import { Iterable } from 'immutable';
+
 import locations from './reducer';
 
 const reducers = combineReducers({
     locations,
 });
 
-const createStore = () => createBasicStore(reducers);
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+    const { createLogger } = require('redux-logger');
+
+    const logger = createLogger({
+        stateTransformer: (state) => {
+            if (Iterable.isIterable(state)) {
+                return state.toJS();
+            }
+            return state;
+        },
+    });
+
+    middlewares.push(logger);
+}
+
+const createStore = () => createBasicStore(reducers, applyMiddleware(...middlewares));
 
 export default createStore;
